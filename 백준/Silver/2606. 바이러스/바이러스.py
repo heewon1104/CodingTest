5446827bd506b4c6e5fdfa3dec0e1cc46f5b10d2
heewon1104 <65596779+heewon1104@users.noreply.github.com>
2024-02-12 19:51:09 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,32 +1,69 @@
 from collections import deque
-
+# BFS로 풀었을때
 n = int(input())
 node = int(input())
+
 arr = []
 for _ in range(n):
-    inputlist = list([0] * n)
-    arr.append(inputlist)
+    arr.append(list([0] * n))
+
+for _ in range(node):
+    a,b = map(int, input().split())
+    a -= 1
+    b -= 1
+    arr[a][b] = 1
+    arr[b][a] = 1
 
 visit = [False] * n
 count = 0
 
-for i in range(node):
-    a, b = map(int,input().split())
-    a-=1
-    b-=1
-    arr[a][b] = 1
-    arr[b][a] = 1
 
-def dfs(index):
-    visit[index] = True
+queue = deque()
+queue.append(0)
+
+while(queue):
+    num = queue.popleft()
+    visit[num] = True
+    
     for i in range(n):
-        if(not visit[i] and arr[index][i] == 1):
-            dfs(i)
-
-dfs(0)
+        if(not visit[i] and arr[num][i] == 1):
+            queue.append(i)
 
 for i in range(n):
-    if(visit[i] == True):
-        count+=1
+     if(visit[i] == True):
+         count+=1
 
 print(count-1)
+    
+
+# DFS로 풀었을때
+# n = int(input())
+# node = int(input())
+# arr = []
+# for _ in range(n):
+#     inputlist = list([0] * n)
+#     arr.append(inputlist)
+
+# visit = [False] * n
+# count = 0
+
+# for i in range(node):
+#     a, b = map(int,input().split())
+#     a-=1
+#     b-=1
+#     arr[a][b] = 1
+#     arr[b][a] = 1
+
+# def dfs(index):
+#     visit[index] = True
+#     for i in range(n):
+#         if(not visit[i] and arr[index][i] == 1):
+#             dfs(i)
+
+# dfs(0)
+
+# for i in range(n):
+#     if(visit[i] == True):
+#         count+=1
+
+# print(count-1)
