e9b64232a74b9af189b4dab54a95d7c9c586e48c
heewon1104 <65596779+heewon1104@users.noreply.github.com>
2024-02-26 13:56:21 +0900

    [Silver I] Title: IOIOI, Time: 344 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,16 +3,18 @@ import sys
 n = int(input())
 m = int(input())
 arr = input()
-checkarr = []
 
-for i in range(n+1):
-    checkarr.append('I')
-    if(i != n):
-        checkarr.append('O')
+cursor, count, result = 0, 0, 0
 
-res = 0
-for i in range(0, m-(2*n)):
-    slicedStr = list(arr[i:i+(2*n+1)])
-    if(slicedStr == checkarr):
-        res+=1
-print(res)
+while cursor < (m - 1):
+    if arr[cursor:cursor + 3] == 'IOI':
+        count += 1
+        cursor += 2
+        if count == n:
+            result += 1
+            count -= 1
+    else:
+        cursor += 1
+        count = 0
+
+print(result)
