ff6acec41ac229192f298637fd2414c695d61bb5
heewon1104 <65596779+heewon1104@users.noreply.github.com>
2024-09-06 14:34:34 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 13248 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() {
+    val s = Scanner(System.`in`)
+
+    var a : Int = s.nextInt()
+    var b : Int = s.nextInt()
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
