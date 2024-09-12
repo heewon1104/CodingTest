f4a4ce116a5926967cdcea4466a75ae1a6d6eed6
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-09-12 13:26:06 +0900

    [Silver II] Title: 부분수열의 합, Time: 296 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,19 +1,16 @@
+import sys
 from itertools import combinations
+# from collections import deque
+# sys.setrecursionlimit(100000) 
+input = sys.stdin.readline
 
-N, S = map(int, input().split())
+N,S = map(int,input().split())
+lst = list(map(int,input().split()))
+cnt = 0
 
-arr = list(map(int, input().split()))
+for i in range(1,N+1):
+    for c in combinations(lst,i):
+        if S == sum(c):
+            cnt += 1
 
-res = []
-count = 0
-
-for i in range(1, N+1):
-    cal = list(combinations(arr, i))
-    res.append(cal)
-
-for i in res:
-    for j in range(len(i)):
-        if(sum(i[j]) == S):
-            count += 1
-
-print(count)
+print(cnt)
