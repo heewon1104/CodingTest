a80896f1e11cae8498729e206fb79c62838e0c57
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-09-19 21:11:54 +0900

    [Gold III] Title: 아침 산책, Time: 1032 ms, Memory: 110192 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -4,31 +4,33 @@ sys.setrecursionlimit(10**7)
 N = int(sys.stdin.readline())
 value = sys.stdin.readline().rstrip('\n')
 edges = list([] for _ in range(N+1))
+Count = 0
 
 for _ in range(N-1):
     num1, num2 = map(int, sys.stdin.readline().split())
     edges[num1].append(num2)
     edges[num2].append(num1)
+    if(value[num1-1] == '1' and value[num2-1] == '1'):
+        Count += 2
 
-Count = 0
 
 def Dfs(node, visited, firstFlag):
-    global Count
+    insideCount = 0
     visited[node] = True
-    
-    if(not firstFlag and value[node-1] == '1'):
-        Count+= 1
-        return
-    
+
     for i in range(len(edges[node])):
         nextnode = edges[node][i]
-        if(not visited[nextnode]):
-            Dfs(nextnode, visited, False)
+        if(value[nextnode-1] == '1'):
+            insideCount += 1
+        elif(not visited[nextnode] and value[nextnode-1] == '0'):
+            insideCount += Dfs(nextnode, visited, False)
+        
+    return insideCount
 
 
+visited = [False] * (N+1)
 for i in range(1, N+1):
-    if(value[i-1] == '1'):
-        visited = [False] * (N+1)
-        Dfs(i, visited, True)
-    
+    if(value[i-1] == '0' and not visited[i]):
+        total = Dfs(i, visited, True)
+        Count += total * (total-1)
 print(Count)
