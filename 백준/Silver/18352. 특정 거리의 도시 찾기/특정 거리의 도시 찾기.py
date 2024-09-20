8b6e9ad5a10abb589a58b14a39091f7fa8d895bf
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-09-20 09:02:54 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1796 ms, Memory: 107236 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,36 +1,33 @@
-from collections import deque
+import sys
+from collections import deque 
 
-N, M, K, X = map(int, input().split())
-
-arr = []
-for i in range(N+1):
-    arr.append(list([] * (N+1)))
+N, M, K, X = map(int, sys.stdin.readline().split())
+road = [[] for _ in range(N+1)]
 
 for i in range(M):
-    x, y = map(int, input().split())
-    arr[x].append(y)
+    start, end = map(int, sys.stdin.readline().split())
+    road[start].append(end)
 
-visit = [-1] * (N+1)
-
-queue = deque()
-queue.append([X, 0])
-
-while(queue):
-    tmp = queue.popleft()
+def Bfs():
+    visited = [-1 for _ in range(N+1)]
+    visited[X] = 0
+    queue = deque()
+    queue.append([X, 0])
     
-    if(visit[tmp[0]] != -1):
-        continue
+    while(queue):
+        current, distance = queue.popleft()
 
-    visit[tmp[0]] = tmp[1]
+        for i in range(len(road[current])):
+            if(visited[road[current][i]] == -1):
+                queue.append([road[current][i], distance+1])
+                visited[road[current][i]] = distance+1
 
-    for i in range(len(arr[tmp[0]])):
-        queue.append([arr[tmp[0]][i], tmp[1]+1])
-
-checknone = True
-for i in range(1, N+1):
-    if(K == visit[i]):
-        checknone = False
-        print(i)
-
-if(checknone):
-    print(-1)
+    count = 0
+    for i in range(1, N+1):
+        if(visited[i] == K):
+            count += 1
+            print(i)
+    
+    if(count == 0):
+        print(-1)
+Bfs()
