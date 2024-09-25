5d0557c2ed0234112344c4cd0cc7c7090ff548cf
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-09-25 10:06:29 +0900

    [Silver I] Title: 영역 구하기, Time: 44 ms, Memory: 32764 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+N, M, K= map(int, sys.stdin.readline().split())
+matrix = [([False] * M) for _ in range(N)]
+result = []
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+for i in range(K):
+    startX, startY, endX, endY = map(int, sys.stdin.readline().split())
+    for y in range(startY, endY):
+        for x in range(startX, endX):
+            matrix[y][x] = True
+
+def Dfs(currentX, currentY):
+    if(matrix[currentY][currentX] == True):
+        return 1
+    
+    count = 1
+    matrix[currentY][currentX] = True
+    for i in range(4):
+        nextX = currentX + dx[i]
+        nextY = currentY + dy[i]
+        if(0 <= nextX < M and 0 <= nextY < N and not matrix[nextY][nextX]):
+            count += Dfs(nextX, nextY)
+    return count
+
+for i in range(N):
+    for j in range(M):
+        if(not matrix[i][j]):
+            result.append(Dfs(j, i))
+
+print(len(result))
+result.sort()
+for num in result:
+    print(num, end=' ')
