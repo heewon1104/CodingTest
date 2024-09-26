8dd12b004abb24266801fa35abb492976305c346
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-09-26 15:45:07 +0900

    [Silver III] Title: 01타일, Time: 312 ms, Memory: 69652 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,13 +1,13 @@
 N = int(input())
+dp = [0] * (N+1)
 
-d = [0] * 1000001
+dp[1] = 1
 
-d[1] = 1
-d[2] = 2
+if(N != 1):
+    dp[2] = 2
 
-if(N < 3):
-    print(d[N])
-else:
-    for i in range(3,N+1):
-        d[i] =(d[i-1]+d[i-2])%15746
-    print(d[N]%15746)
+if(N > 2):
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1] + dp[i-2])%15746
+
+print(dp[N])
