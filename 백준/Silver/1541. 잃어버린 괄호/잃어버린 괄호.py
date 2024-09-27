b3a57e24b86e0501c4b4fd120b36fb9ac43445b6
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-09-28 08:56:01 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,21 +1,27 @@
-arr = input().split('-')
+import sys
 
-res = 0
-startidx = 0
-if(arr[0] == ''):
-    tmp = arr[1].split('+')
-    for i in range(len(tmp)):
-        res -= int(tmp[i])
-    startidx = 2
+calculate = list(sys.stdin.readline().rstrip('\n').split('-'))
+total = sys.maxsize
+
+if(len(calculate[0]) == 0):
+     calculate[0] = '0'
+
+for i in range(len(calculate)):
+    calculate[i] = sum(list(map(int, calculate[i].split('+'))))
+
+
+if(len(calculate) == 1):
+    print(calculate[0])
+
 else:
-    tmp = arr[0].split('+')
-    for i in range(len(tmp)):
-        res += int(tmp[i])
-    startidx = 1
-
-for i in range(startidx, len(arr)):
-    tmp = arr[i].split('+')
-    for j in range(len(tmp)):
-        res -= int(tmp[j])
-
-print(res)
+    for length in range(1, len(calculate)):
+        for i in range(1, len(calculate)+1):
+            tmpsum = calculate[0]
+            for j in range(1, len(calculate)):
+                if(i<=j<=i+length):
+                    tmpsum -= calculate[j]
+                else:
+                    tmpsum += calculate[j]
+            total = min(total, tmpsum)
+        
+    print(total)
