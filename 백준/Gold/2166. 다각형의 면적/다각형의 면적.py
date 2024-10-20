a167974498737ae1e58b4a1b4a415fbab6128922
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-10-21 07:59:06 +0900

    [Gold V] Title: 다각형의 면적, Time: 104 ms, Memory: 110420 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
@@ -6,6 +6,8 @@ dot = [(list(map(int, sys.stdin.readline().split()))) for _ in range(N)]
 dot.append(dot[0])
 res = 0
 
+# 다각형의 면적 구하는 신발끈 공식
+# abs(0.5 * ∑(xi*y(i+1)−yi*x(i+1)))
 for i in range(N):
     res += dot[i][0]*dot[i+1][1] - dot[i][1]*dot[i+1][0]
 print(abs(res*0.5))
