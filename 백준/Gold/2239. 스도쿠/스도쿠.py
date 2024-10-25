9f0f824ca432c4d9922332d4a535cf3d6f2114fa
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-10-25 13:35:48 +0900

    [Gold IV] Title: 스도쿠, Time: 4608 ms, Memory: 151280 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,50 +1,44 @@
 import sys
-graph = []
-blank = []
 
+board = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(9)]
+ZeroIdx = []
 for i in range(9):
-    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+   for j in range(9):
+       if board[i][j] == 0:
+           ZeroIdx.append((i, j))
 
-for i in range(9):
-    for j in range(9):
-        if graph[i][j] == 0:
-            blank.append((i, j))
-
-def checkRow(x, a):
-    for i in range(9):
-        if a == graph[x][i]:
-            return False
-    return True
-
-def checkCol(y, a):
-    for i in range(9):
-        if a == graph[i][y]:
-            return False
-    return True
-
-def checkRect(x, y, a):
-    nx = x // 3 * 3
-    ny = y // 3 * 3
+def CheckSector(x, y, num):
+    boardX = x // 3 * 3
+    boardY = y // 3 * 3
     for i in range(3):
         for j in range(3):
-            if a == graph[nx+i][ny+j]:
+            if board[boardY + i][boardX + j] == num:  # board[boardY + i][boardX + j]로 수정
                 return False
     return True
 
+def CheckRow(y, num):
+    for i in range(9):
+        if board[y][i] == num:  
+            return False
+    return True
 
-def dfs(idx):
-    if idx == len(blank):
+def CheckColumn(x, num):
+    for i in range(9):
+        if board[i][x] == num:  
+            return False
+    return True
+
+def DFS(idx):
+    if idx == len(ZeroIdx):
         for i in range(9):
-            print(''.join(map(str, graph[i])))
-        exit(0)
-
+            print(''.join(map(str, board[i])))
+        exit(0) 
+    
+    y, x = ZeroIdx[idx]  # x, y 순서를 맞추기 위해 y, x로 설정
     for i in range(1, 10):
-        x = blank[idx][0]
-        y = blank[idx][1]
+        if CheckColumn(x, i) and CheckRow(y, i) and CheckSector(x, y, i):
+            board[y][x] = i
+            DFS(idx + 1)
+            board[y][x] = 0
 
-        if checkRow(x, i) and checkCol(y, i) and checkRect(x, y, i):
-            graph[x][y] = i
-            dfs(idx+1)
-            graph[x][y] = 0
-
-dfs(0)
+DFS(0)
