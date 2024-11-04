2bf76131a698f556bef8e9e70b20851cccc2c169
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-11-04 12:53:14 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 164 ms, Memory: 116704 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,35 +1,30 @@
+# 녹색 옷 입은 애가 젤다지?
 import sys
-import heapq
+from collections import deque
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 TestCount = 1
 
-def searchGraph():
-    queue = []
-    heapq.heappush(queue, (board[0][0], 0, 0))
+def serarchGraph():
+    queue = deque()
+    queue.append([0, 0])
     visited[0][0] = board[0][0]
 
-    while queue:
-        currentCost, currentX, currentY = heapq.heappop(queue)
-
-        if currentX == N - 1 and currentY == N - 1:
-            return currentCost
-
+    while(queue):
+        currentX, currentY = queue.popleft()
         for i in range(4):
-            nextX, nextY = currentX + dx[i], currentY + dy[i]
-            if 0 <= nextX < N and 0 <= nextY < N:
-                nextCost = currentCost + board[nextY][nextX]
-                if nextCost < visited[nextY][nextX]:
-                    visited[nextY][nextX] = nextCost
-                    heapq.heappush(queue, (nextCost, nextX, nextY))
+            nextX, nextY = currentX+dx[i], currentY+dy[i]
+            if(0 <= nextX < N and 0 <= nextY < N and visited[nextY][nextX] > visited[currentY][currentX] + board[nextY][nextX]):
+                visited[nextY][nextX] = visited[currentY][currentX] + board[nextY][nextX]
+                queue.append([nextX, nextY])
 
-while True:
+while(1):
     N = int(sys.stdin.readline())
-    if N == 0:
+    if(N == 0):
         break
-    board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    visited = [[sys.maxsize] * N for _ in range(N)]
-    result = searchGraph()
-    print(f"Problem {TestCount}: {result}")
+    board = [(list(map(int, sys.stdin.readline().split()))) for _ in range(N)]
+    visited = [([sys.maxsize] * (N)) for _ in range(N)]
+    serarchGraph()
+    print(f"Problem {TestCount}: {visited[N-1][N-1]}")
     TestCount += 1
