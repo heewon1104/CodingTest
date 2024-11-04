d40cfd365b04a8c844bb6709b3e8590e425b7830
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-11-04 12:55:00 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 164 ms, Memory: 111900 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,23 +1,25 @@
 # 녹색 옷 입은 애가 젤다지?
 import sys
-from collections import deque
+import heapq
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 TestCount = 1
 
 def serarchGraph():
-    queue = deque()
-    queue.append([0, 0])
+    heap = []
+    heapq.heappush(heap, [board[0][0], 0, 0])
     visited[0][0] = board[0][0]
 
-    while(queue):
-        currentX, currentY = queue.popleft()
+    while(heap):
+        currnetCost, currentX, currentY = heapq.heappop(heap)
+        if(currentX == N-1 and currentY == N-1):
+            return currnetCost
         for i in range(4):
             nextX, nextY = currentX+dx[i], currentY+dy[i]
-            if(0 <= nextX < N and 0 <= nextY < N and visited[nextY][nextX] > visited[currentY][currentX] + board[nextY][nextX]):
-                visited[nextY][nextX] = visited[currentY][currentX] + board[nextY][nextX]
-                queue.append([nextX, nextY])
+            if(0 <= nextX < N and 0 <= nextY < N and visited[nextY][nextX] > currnetCost + board[nextY][nextX]):
+                visited[nextY][nextX] = currnetCost + board[nextY][nextX]
+                heapq.heappush(heap, [visited[nextY][nextX], nextX, nextY])
 
 while(1):
     N = int(sys.stdin.readline())
@@ -25,6 +27,5 @@ while(1):
         break
     board = [(list(map(int, sys.stdin.readline().split()))) for _ in range(N)]
     visited = [([sys.maxsize] * (N)) for _ in range(N)]
-    serarchGraph()
-    print(f"Problem {TestCount}: {visited[N-1][N-1]}")
+    print(f"Problem {TestCount}: {serarchGraph()}")
     TestCount += 1
