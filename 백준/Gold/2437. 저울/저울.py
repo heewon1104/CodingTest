93e27c7c6e78147d80b55fb5c9d4e3987f0a4e2c
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-11-20 18:31:11 +0900

    [Gold II] Title: 저울, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.py
@@ -0,0 +1,12 @@
+import sys
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+arr.sort()
+
+total = 0
+for i in range(N):
+    if total + 1 < arr[i]:
+        break
+    total += arr[i]
+
+print(total + 1)
