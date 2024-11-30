d88763a3dc724be216d44e84b8babde98961f793
김희원 <65596779+heewon1104@users.noreply.github.com>
2024-11-30 15:20:47 +0900

    [Gold IV] Title: LCS 3, Time: 172 ms, Memory: 119924 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -1,22 +1,24 @@
+# LCS 3
 import sys
+strArr = []
+for _ in range(3):
+    strArr.append(list(sys.stdin.readline().rstrip('\n')))
 
-str1 = list(sys.stdin.readline().rstrip('\n'))
-str2 = list(sys.stdin.readline().rstrip('\n'))
-str3 = list(sys.stdin.readline().rstrip('\n'))
+length1, length2, length3 = len(strArr[2]), len(strArr[1]), len(strArr[0])
 
-N = len(str1)
-M = len(str2)
-L = len(str3)
+dp = []
+for _ in range(length1+1):
+    apeendArr = [([0] * (length3+1)) for _ in range(length2+1)]
+    dp.append(apeendArr)
 
-dp = [([([0]*(M+1)) for _ in range(N+1)]) for _ in range(L+1)]
-
-for k in range(L):
-    for i in range(N):
-        for j in range(M):
-            if(str1[i] == str2[j] == str3[k]):
-                dp[k+1][i+1][j+1] = dp[k][i][j] + 1
-
+for i in range(length1):
+    for j in range(length2):
+        for k in range(length3):
+            if(strArr[0][k] == strArr[1][j] == strArr[2][i]):
+                dp[i+1][j+1][k+1] = dp[i][j][k] + 1
             else:
-                dp[k+1][i+1][j+1] = max(dp[k+1][i+1][j],  dp[k+1][i][j+1],  dp[k][i+1][j+1])
+                dp[i+1][j+1][k+1] = max(dp[i][j+1][k+1], dp[i+1][j][k+1], dp[i+1][j+1][k])
 
-print(dp[L][N][M])
+
+print(dp[length1][length2][length3])
+
