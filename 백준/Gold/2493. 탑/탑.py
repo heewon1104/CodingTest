cc51fa1065d2c78131577479a672e2272c8a1cd3
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-14 21:48:12 +0900

    [Gold V] Title: 탑, Time: 280 ms, Memory: 212708 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,20 +1,21 @@
 N = int(input())
 arr = list(map(int, input().split()))
-Stack = [[arr[0],0]]
-print(0, end=' ')
+stack = []
 
-for i in range(1, N):
+prev = arr[N-1]
+res = [0] * N
 
-    while(1):
-        if(len(Stack) == 0):
-            print(0, end=' ')
-            break
-         # Stack의 Top이 배열의 현재 값보다 작으면
-        if(Stack[-1][0] < arr[i]):
-            #해당 값은 필요 없으므로 pop, 현재값 push
-            Stack.pop()
-        else:
-            print(Stack[-1][1] +1, end=' ')
-            break
-    
-    Stack.append([arr[i], i])
+stack.append((prev, N))
+for i in range(N-2, -1, -1):
+    if(prev < arr[i]):
+        while(len(stack) != 0  and stack[-1][0] < arr[i]):
+            popped, idx = stack.pop()
+            res[idx-1] = i+1
+    prev = arr[i]
+    stack.append((arr[i], i+1))
+
+for i in res:
+    print(i, end=' ')
+
+    import sys
+input = sys.stdin.readline
