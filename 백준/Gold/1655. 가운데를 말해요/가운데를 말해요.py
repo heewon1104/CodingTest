37168712ad3b0b705c4e623ea2887c0add842cb8
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-15 02:56:49 +0900

    [Gold II] Title: 가운데를 말해요, Time: 296 ms, Memory: 120932 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -3,21 +3,26 @@ import sys
 
 N = int(input())
 
-large = []  
-small = [] 
+largeHeap = []
+smallHeap = []
 
 for i in range(N):
-    num = int(sys.stdin.readline())
-
-    if len(large) == len(small):
-        heapq.heappush(small, -num) 
+    inputNum = int(sys.stdin.readline())
+    if(not largeHeap or largeHeap[0] < inputNum):
+        heapq.heappush(largeHeap, inputNum)
     else:
-        heapq.heappush(large, num) 
+        heapq.heappush(smallHeap, -inputNum)
     
-    if large and (-small[0] > large[0]):
-        small_top = -heapq.heappop(small)
-        large_top = heapq.heappop(large)
-        heapq.heappush(small, -large_top)
-        heapq.heappush(large, small_top)
+    if((len(largeHeap) + len(smallHeap)) % 2 == 1):
+        if(len(largeHeap) > len(smallHeap)):
+            print(largeHeap[0])
+        else:
+            print(-smallHeap[0])
+    else:
+        if(len(largeHeap) > len(smallHeap)):
+            heapq.heappush(smallHeap, -heapq.heappop(largeHeap))
+        elif(len(largeHeap) < len(smallHeap)):
+            heapq.heappush(largeHeap, -heapq.heappop(smallHeap))
 
-    print(-small[0])
+        print(min(largeHeap[0], -smallHeap[0]))
+
