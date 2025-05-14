28ef2a0be3f24d463d571ec6c2f686a3f91319ff
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-14 21:48:51 +0900

    [Gold V] Title: 탑, Time: 276 ms, Memory: 212712 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -5,17 +5,15 @@ stack = []
 prev = arr[N-1]
 res = [0] * N
 
-stack.append((prev, N))
+stack.append((prev, N-1))
 for i in range(N-2, -1, -1):
+
     if(prev < arr[i]):
         while(len(stack) != 0  and stack[-1][0] < arr[i]):
             popped, idx = stack.pop()
-            res[idx-1] = i+1
+            res[idx] = i+1
     prev = arr[i]
-    stack.append((arr[i], i+1))
+    stack.append((arr[i], i))
 
 for i in res:
     print(i, end=' ')
-
-    import sys
-input = sys.stdin.readline
