cb24d0e872d6c2109a12b296efd499cfe3d257c9
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-15 01:17:34 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 120 ms, Memory: 111988 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,21 +1,17 @@
 from collections import deque
 
-N, K = map(int, input().split())
-
+N, M = map(int, input().split())
 queue = deque()
+
 for i in range(N):
     queue.append(i+1)
 
-count = 0
-print('<' , end='')
-while(len(queue) > 1):
-    popped = queue.popleft()
-    count += 1
+print("<", end="")
 
-    if(count != K):
-        queue.append(popped)
-    else:
-        print(popped, end=', ')
-        count = 0
-    
-print(queue.popleft(), end='>\n')
+while(len(queue) > 1):
+    for i in range(M-1):
+        queue.append(queue.popleft())
+    print(queue.popleft(), end=", ")
+
+
+print(queue.popleft(), end=">")
