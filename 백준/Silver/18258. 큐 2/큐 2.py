4f95b47dccf7a351c46376d0979d9ff5375eea43
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-15 01:25:23 +0900

    [Silver IV] Title: 큐 2, Time: 792 ms, Memory: 253848 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -2,38 +2,37 @@ from collections import deque
 import sys
 
 N = int(input())
-
 queue = deque()
 
 for i in range(N):
-    cmd = list(sys.stdin.readline().split())
+    inputCommand = list(sys.stdin.readline().split())
 
-    if(cmd[0] == 'push'):
-        queue.append(cmd[1])
+    if(inputCommand[0] == "push"):
+        queue.append(inputCommand[1])
 
-    elif(cmd[0] == 'pop'):
-        if(len(queue) == 0):
+    elif(inputCommand[0] == "pop"):
+        if(not queue):
             print(-1)
         else:
             print(queue.popleft())
 
-    elif(cmd[0] == 'size'):
+    elif(inputCommand[0] == "size"):
         print(len(queue))
 
-    elif(cmd[0] == 'empty'):
-        if(len(queue) == 0):
+    elif(inputCommand[0] == "empty"):
+        if(not queue):
             print(1)
         else:
             print(0)
 
-    elif(cmd[0] == 'front'):
-        if(len(queue) == 0):
+    elif(inputCommand[0] == "front"):
+        if(not queue):
             print(-1)
         else:
             print(queue[0])
 
     else:
-        if(len(queue) == 0):
+        if(not queue):
             print(-1)
         else:
             print(queue[-1])
