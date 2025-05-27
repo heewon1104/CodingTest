d3499cb48b3d9c7d7f0fda269cc10654200fd922
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-27 22:59:09 +0900

    [Gold III] Title: 줄 세우기, Time: 288 ms, Memory: 117380 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,32 +1,28 @@
-import sys
 from collections import deque
 
-N, M = map(int, sys.stdin.readline().split())
-student = [[] for _ in range(N+1)]
-degree = [0 for _ in range(N+1)]
-for i in range(M):
-    short, long = map(int, sys.stdin.readline().split())
-    student[short].append(long)
-    degree[long] += 1
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+degree = [0] * (N+1)
+visit = [False] * (N+1)
 
-def topology_sort():
-    result = []
-    queue = deque()
-    for i in range(1, N+1):
-        if(degree[i] == 0):
-            queue.append(i)
-    
-    while(queue):
-        popped = queue.popleft()
-        result.append(popped)
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    degree[end] += 1
 
-        for i in range(len(student[popped])):
-            degree[student[popped][i]] -= 1
-            if(degree[student[popped][i]] == 0):
-                queue.append(student[popped][i])
+queue = deque()
+for i in range(1, N+1):
+    if(degree[i] == 0):
+        queue.append(i)
 
-    for i in result:
-        print(i, end=' ')
-    print()
+while(queue):
+    popped = queue.popleft()
+    visit[popped] = True
+    print(popped, end=' ')
+    for i in range(len(graph[popped])):
+        next = graph[popped][i]
+        degree[next] -= 1
+        if(not visit[next] and degree[next]==0):
+            queue.append(next)
 
-topology_sort()
+
