0d67cffda90f95e3efa9fdc207946850a72f02dd
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-27 17:00:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 516 ms, Memory: 176144 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,35 +1,26 @@
-import sys
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
 
-N, M = map(int, sys.stdin.readline().split())
-edge = [[] for _ in range(N+1)]
-parent = [i for i in range(N+1)]
-mergedCount = 0
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
-for i in range(M):
-    start, end = map(int, sys.stdin.readline().split())
-    edge[start].append(end)
-    edge[end].append(start)
+roots = [i for i in range(N+1)]
+marged = 0
 
 def findParent(node):
-    while(parent[node] != node):
-        node = parent[node]
-    return parent[node]
-
-def UnionSet(node1, node2):
-    global mergedCount
-
-    node1root = findParent(node1)
-    node2root = findParent(node2)
-
-    if(node1root != node2root):
-        if(node1root < node2root):
-            parent[node2root] = node1root
-        else:
-            parent[node1root] = node2root
-        mergedCount += 1
+    while(roots[node] != node):
+        node = roots[node]
+    return node
 
 for i in range(1, N+1):
-    for j in range(len(edge[i])):
-        UnionSet(i, edge[i][j])
+    for j in range(len(graph[i])):
+        if(findParent(i) != findParent(graph[i][j])):
+            if(roots[i] < roots[graph[i][j]]):
+                roots[graph[i][j]] = roots[i]
+            else:
+                roots[i] = roots[graph[i][j]]
+            marged += 1
 
-print(N - mergedCount)
+print(N-marged)
