91e9e2bf301989558b7852e248ea4d0c4917ceda
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-27 23:01:04 +0900

    [Gold III] Title: 줄 세우기, Time: 240 ms, Memory: 117048 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,7 +3,6 @@ from collections import deque
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
 degree = [0] * (N+1)
-visit = [False] * (N+1)
 
 for _ in range(M):
     start, end = map(int, input().split())
@@ -17,12 +16,11 @@ for i in range(1, N+1):
 
 while(queue):
     popped = queue.popleft()
-    visit[popped] = True
     print(popped, end=' ')
     for i in range(len(graph[popped])):
         next = graph[popped][i]
         degree[next] -= 1
-        if(not visit[next] and degree[next]==0):
+        if(degree[next]==0):
             queue.append(next)
 
 
