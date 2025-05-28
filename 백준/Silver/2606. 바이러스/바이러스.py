36539acd66ed79e5eacfaa9cf3014f13758b7b07
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-29 03:12:41 +0900

    [Silver III] Title: 바이러스, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,38 +1,24 @@
-import sys
+N = int(input())
+M = int(input())
 
-N = int(sys.stdin.readline())
-M = int(sys.stdin.readline())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    node1, node2 = map(int, input().split())
+    graph[node1].append(node2)
+    graph[node2].append(node1)
 
-edge = [[] for _ in range(N+1)]
-parent = [i for i in range(N+1)]
-ConnectCount = 0
+visit = [False] * (N+1)
+visit[1] = True
+infected = 0
 
-for i in range(M):
-    start, end = map(int, sys.stdin.readline().split())
-    edge[start].append(end)
-    edge[end].append(start)
+def DFS(current):
+    global infected
+    
+    for next in graph[current]:
+        if(not visit[next]):
+            visit[next] = True
+            infected += 1
+            DFS(next)
 
-def findParent(node):
-    while(parent[node] != node):
-        node = parent[node]
-    return parent[node]
-
-def UnionSet(node1, node2):
-    node1root = findParent(node1)
-    node2root = findParent(node2)
-
-    if(node1root != node2root):
-        if(node1root < node2root):
-            parent[node2root] = node1root
-        else:
-            parent[node1root] = node2root
-
-for i in range(1, N+1):
-    for j in range(len(edge[i])):
-        UnionSet(i, edge[i][j])
-
-for i in range(1, N+1):
-    if(findParent(i) == 1):
-        ConnectCount += 1
-
-print(ConnectCount-1)
+DFS(1)
+print(infected)
