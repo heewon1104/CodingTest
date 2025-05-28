8161eee4389702f3ea57797d8bc03dd965269c3d
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-05-29 05:47:24 +0900

    [Gold IV] Title: 빙산, Time: 4332 ms, Memory: 34620 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**6)  # 재귀 깊이를 줄임
+sys.setrecursionlimit(10**6) 
 
 N, M = map(int, sys.stdin.readline().split())
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
@@ -7,7 +7,6 @@ board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-# DFS 대신 스택을 이용한 반복 구조로 변경
 def checkDivide(X, Y, visited):
     stack = [(X, Y)]
     visited[Y][X] = True
@@ -21,8 +20,7 @@ def checkDivide(X, Y, visited):
                 stack.append((nextX, nextY))
 
 def melted():
-    # 빙산을 녹이는 함수
-    new_board = [row[:] for row in board]  # 녹은 상태를 바로 반영하기 위해 복사본 사용
+    new_board = [row[:] for row in board] 
     for i in range(N):
         for j in range(M):
             if board[i][j] > 0:
@@ -42,7 +40,6 @@ while True:
     count = 0
     isAllMelted = True
 
-    # 빙산 구역 나누기 (DFS 반복 구조 사용)
     for i in range(N):
         for j in range(M):
             if board[i][j] > 0 and not visited[i][j]:
@@ -50,7 +47,6 @@ while True:
                 count += 1
                 isAllMelted = False
 
-    # 두 개 이상의 빙산이 존재하거나 빙산이 다 녹았을 경우 종료
     if count >= 2:
         print(Year)
         break
@@ -58,6 +54,5 @@ while True:
         print(0)
         break
 
-    # 빙산을 녹이고 board 업데이트
     board = melted()
     Year += 1
