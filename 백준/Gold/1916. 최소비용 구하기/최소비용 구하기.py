263e113dc3091c8dee22807e857d99905babb176
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-06-02 16:07:42 +0900

    [Gold V] Title: 최소비용 구하기, Time: 180 ms, Memory: 122956 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,32 +1,29 @@
-import sys
 import heapq
+import sys
 
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
-arcs = [[] for _ in range(N+1)]
-heap = []
-costs = [sys.maxsize for _ in range(N+1)]
 
+graph = [[] for _ in range(N+1)]
 for _ in range(M):
-    start, end, value = map(int, sys.stdin.readline().split())
-    arcs[start].append([end, value])
+    inputStart, inputEnd, inputCost = map(int, sys.stdin.readline().split())
+    graph[inputStart].append((inputCost, inputEnd))
 
-startNode, endNode = map(int, sys.stdin.readline().split())
+start, end = map(int, sys.stdin.readline().split())
 
-def Dijkstra():
-    heapq.heappush(heap, [0, startNode])
-    costs[startNode] = 0
+cost = [sys.maxsize] * (N+1)
+heap = []
+heapq.heappush(heap, (0, start))
+cost[start] = 0
 
-    while(heap):
-        popvalue, popNode = heapq.heappop(heap) 
-        if(popvalue > costs[popNode]):
-            continue
-        for i in range(len(arcs[popNode])):
-            nextNode = arcs[popNode][i]
-            if(costs[nextNode[0]] > nextNode[1] + costs[popNode]):
-                costs[nextNode[0]] = nextNode[1] + costs[popNode]
-                heapq.heappush(heap, [nextNode[1], nextNode[0]])
+while(heap):
+    currentCost, current = heapq.heappop(heap)
+    if(current == end):
+        break
     
-    print(costs[endNode])
+    for nextCost, next in graph[current]:
+        if(nextCost + currentCost < cost[next]):
+            cost[next] = nextCost + currentCost
+            heapq.heappush(heap, (nextCost + currentCost, next))
 
-Dijkstra()
+print(cost[end])
