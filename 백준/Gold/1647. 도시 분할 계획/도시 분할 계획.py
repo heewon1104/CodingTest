7c2d47397cacc425552d168d95dde92c095b271e
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-06-02 17:07:17 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3932 ms, Memory: 260236 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,44 +1,34 @@
-# 도시 분할 계획
-import sys
 import heapq
+import sys
+
 N, M = map(int, sys.stdin.readline().split())
-parent = [i for i in range(N+1)]
 heap = []
-res = 0
 
 for _ in range(M):
     start, end, cost = map(int, sys.stdin.readline().split())
-    heapq.heappush(heap, [cost, start, end])
+    heapq.heappush(heap, (cost, start, end))
 
-def FindRoot(node):
-    while(node != parent[node]):
-        node = parent[node]
-    return node
+roots = [i for i in range(N+1)]
+result = 0
+countEdge = 0
 
-rank = [0] * (N+1)  
+def findRoots(node):
+    if(roots[node] != node):
+        roots[node] = findRoots(roots[node])
+    return roots[node]
 
-def CompareRoot(node1, node2):
-    root1 = FindRoot(node1)
-    root2 = FindRoot(node2)
-    
-    if root1 != root2:
-        if rank[root1] > rank[root2]:
-            parent[root2] = root1
-        elif rank[root1] < rank[root2]:
-            parent[root1] = root2
+while(countEdge < N-2):
+    currentCost, currentStart, currentEnd= heapq.heappop(heap)
+    rootStart = findRoots(currentStart)
+    rootEnd = findRoots(currentEnd)
+
+    if(rootStart != rootEnd):
+        if(rootStart < rootEnd):
+            roots[rootEnd] = rootStart
         else:
-            parent[root2] = root1
-            rank[root1] += 1
-        return True
-    return False
+            roots[rootStart] = rootEnd
 
-
-total = 0
-lastCost = -1
-while(heap):
-    currentCost, currentStart, currentEnd = heapq.heappop(heap)
-    if(CompareRoot(currentStart, currentEnd)):
-        total += currentCost
-        lastCost = currentCost
-
-print(total - lastCost)
+        result += currentCost
+        countEdge += 1
+        
+print(result)
