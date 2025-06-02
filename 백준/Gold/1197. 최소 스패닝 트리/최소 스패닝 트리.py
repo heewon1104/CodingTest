84e9ecda37065b7992983683440639b0791bbf32
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-06-02 16:55:39 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 372 ms, Memory: 55812 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,39 +2,33 @@ import sys
 import heapq
 sys.setrecursionlimit(10**7)
 
-V, E = map(int,  sys.stdin.readline().split())
-edges = []
-parent = list(i for i in range(0, V+1))
+V, E = map(int, sys.stdin.readline().split())
+heap = []
 
-for i in range(E):
-    start, end, value = map(int, sys.stdin.readline().split())
-    heapq.heappush(edges, (value, start, end))
+for _ in range(E):
+    start, end, cost = map(int, sys.stdin.readline().split())
+    heapq.heappush(heap, [cost, start, end])
 
-# 무도건 값이 적은 노드를 parent로 설정하여 연결된 가장 작은 노드까지 이동
-def findParent(node):
-    if(parent[node] != node):
-        parent[node] = findParent(parent[node])
-    return parent[node]
+roots = [i for i in range(V+1)]
+edgeCount = 0
+result = 0
 
-def union_parent(a, b):
-    a = findParent(a)
-    b = findParent(b)
+def findRoot(node):
+    if(roots[node] != node):
+        roots[node] = findRoot(roots[node])
+    return roots[node]
 
-    if a < b: # 작은 쪽이 부모가 된다. (한 집합 관계라서 부모가 따로 있는 건 아님)
-        parent[b] = a
-    else:
-        parent[a] = b        
+while(edgeCount < V-1):
+    poppedCost, poppedStart, poppedEnd = heapq.heappop(heap)
+    startRoot = findRoot(poppedStart)
+    endRoot = findRoot(poppedEnd)
 
-def same_parent(a, b):
-    return findParent(a) == findParent(b)
+    if(startRoot != endRoot):
+        if(startRoot < endRoot):
+            roots[endRoot] = poppedStart
+        elif(startRoot > endRoot):
+            roots[startRoot] = poppedEnd
+        result += poppedCost
+        edgeCount += 1
 
-
-
-answer = 0
-for i in range(len(edges)):
-    value, a, b = heapq.heappop(edges)
-    if not same_parent(a, b):
-        union_parent(a, b)
-        answer += value
-
-print(answer)
+print(result)
