5fe1096aef9f20132054ab28395ac10cb4e88367
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-06-03 19:33:24 +0900

    [Gold V] Title: 토마토, Time: 588 ms, Memory: 235232 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,47 +1,52 @@
 import sys
 from collections import deque
 
-M, N, H = map(int, sys.stdin.readline().split())
-boxes = [[] for _ in range(H)]
-dx = [1, 0, -1, 0, 0, 0]
-dy = [0, 1, 0, -1, 0, 0]
-dh = [0, 0 ,0, 0, 1, -1]
+input = sys.stdin.readline
+INF = 10**9
+
+M, N, H = map(int, input().split())
+boxes = [[[0] * M for _ in range(N)] for _ in range(H)]
 queue = deque()
 
-for h in range(H):
-    for y in range(N):
-        inputArr = list(map(int, sys.stdin.readline().split()))
-        boxes[h].append(inputArr)
-        for x in range(M):
-            if(boxes[h][y][x] == 1):
-                queue.append([x, y, h, 0])
+zero_count = 0
 
-def CheckBoxes():
-    for h in range(H):
-        for y in range(N):
-            for x in range(M):
-                if(boxes[h][y][x] == 0):
-                    return False
-    return True
+dx = [1, -1, 0,  0, 0,  0]
+dy = [0,  0, 1, -1, 0,  0]
+dz = [0,  0, 0,  0, 1, -1]
+
+for z in range(H):
+    for y in range(N):
+        row = list(map(int, input().split()))
+        for x in range(M):
+            val = row[x]
+            boxes[z][y][x] = val
+            if val == 0:
+                zero_count += 1
+            elif val == 1:
+                queue.append((x, y, z, 0))
+
+if zero_count == 0:
+    print(0)
+    sys.exit(0)
 
 result = 0
-while(queue):
-    check = CheckBoxes()
-    if(check):
-        break
 
-    currentX, currentY, currentH, currentTime = queue.popleft()
+while queue:
+    x, y, z, t = queue.popleft()
+    result = max(result, t)
+
     for i in range(6):
-        nextH = currentH + dh[i]
-        nextY = currentY + dy[i]
-        nextX = currentX + dx[i]
-        
-        if(0<=nextH<H and 0<=nextY< N and 0<=nextX< M and boxes[nextH][nextY][nextX] == 0):
-            boxes[nextH][nextY][nextX] = 1
-            queue.append([nextX, nextY, nextH, currentTime + 1])
-            result = max(result, currentTime + 1)
+        nx = x + dx[i]
+        ny = y + dy[i]
+        nz = z + dz[i]
 
-if(not CheckBoxes()):
+        if 0 <= nx < M and 0 <= ny < N and 0 <= nz < H:
+            if boxes[nz][ny][nx] == 0:
+                boxes[nz][ny][nx] = 1
+                zero_count -= 1
+                queue.append((nx, ny, nz, t + 1))
+
+if zero_count > 0:
     print(-1)
 else:
     print(result)
