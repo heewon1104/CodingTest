6b65d25bb578346c762be2f0ddda77899d08f994
김희원 <65596779+heewon1104@users.noreply.github.com>
2025-06-03 19:28:09 +0900

    [Gold V] Title: 토마토, Time: 1316 ms, Memory: 213988 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,47 +1,47 @@
 import sys
-from collections import deque 
+from collections import deque
 
 M, N, H = map(int, sys.stdin.readline().split())
-boxes = [[list(map(int, sys.stdin.readline().split())) for _ in range(N)] for _ in range(H)]
+boxes = [[] for _ in range(H)]
 dx = [1, 0, -1, 0, 0, 0]
 dy = [0, 1, 0, -1, 0, 0]
-dh = [0,0 ,0, 0, 1, -1]
+dh = [0, 0 ,0, 0, 1, -1]
+queue = deque()
 
-def checkExsist(num):
-    for h in range(H):
-            for y in range(N):
-                for x in range(M):
-                    if(boxes[h][y][x] == num):
-                        return True
-    return False
+for h in range(H):
+    for y in range(N):
+        inputArr = list(map(int, sys.stdin.readline().split()))
+        boxes[h].append(inputArr)
+        for x in range(M):
+            if(boxes[h][y][x] == 1):
+                queue.append([x, y, h, 0])
 
-
-def bfs():
-    queue = deque()
-    
-    result = 0
+def CheckBoxes():
     for h in range(H):
         for y in range(N):
             for x in range(M):
-                if(boxes[h][y][x] == 1):
-                    queue.append([h, y, x, 0])
+                if(boxes[h][y][x] == 0):
+                    return False
+    return True
 
-    while(queue):
-        h, y, x, day = queue.popleft()
+result = 0
+while(queue):
+    check = CheckBoxes()
+    if(check):
+        break
 
-        for i in range(6):
-            nextH = h + dh[i]
-            nextY = y + dy[i]
-            nextX = x + dx[i]
+    currentX, currentY, currentH, currentTime = queue.popleft()
+    for i in range(6):
+        nextH = currentH + dh[i]
+        nextY = currentY + dy[i]
+        nextX = currentX + dx[i]
+        
+        if(0<=nextH<H and 0<=nextY< N and 0<=nextX< M and boxes[nextH][nextY][nextX] == 0):
+            boxes[nextH][nextY][nextX] = 1
+            queue.append([nextX, nextY, nextH, currentTime + 1])
+            result = max(result, currentTime + 1)
 
-            if(nextH >= 0 and nextH < H and nextY >= 0 and nextY < N and nextX>=0 and nextX<M and boxes[nextH][nextY][nextX] == 0):
-                queue.append([nextH, nextY, nextX, day+1])
-                boxes[nextH][nextY][nextX] = 1
-                result = day+1
-    
-    if(checkExsist(0)):
-        print(-1)
-    else:
-        print(result)
-
-bfs()
+if(not CheckBoxes()):
+    print(-1)
+else:
+    print(result)
